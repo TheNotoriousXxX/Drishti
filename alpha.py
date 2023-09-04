50f0b5ebe357f7b382d7ca0add8d045e5ff8e6fa
Shubham Gurung Lama <69759381+TheNotoriousXxX@users.noreply.github.com>
2023-09-04 14:26:08 +0545

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,25 @@
+#image captioning betaa
+
+#pip install git+https://github.com/huggingface/transformers.git
+
+
+url = 'https://media.newyorker.com/cartoons/63dc6847be24a6a76d90eb99/master/w_1160,c_limit/230213_a26611_838.jpg'
+image = Image.open(requests.get(url, stream=True).raw).convert('RGB')  
+# display(image.resize((596, 437)))
+
+from transformers import AutoProcessor, Blip2ForConditionalGeneration
+import torch
+
+processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16)
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+model.to(device)
+
+inputs = processor(image, return_tensors="pt").to(device, torch.float16)
+
+generated_ids = model.generate(**inputs, max_new_tokens=20)
+generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+print(generated_text)
+
+
